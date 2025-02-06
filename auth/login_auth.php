e4963fe0699dc1d5adfdf7f9bba1b1a5e905fb95
evamelisa-rpl <evamelisa2007@gmail.com>
2025-02-06 11:32:03 +0700

    menambahkan login auth

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('../config.php');
+require_once("../config.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
@@ -11,25 +11,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        
+        //Verifikasi password
         if (password_verify($password, $row["password"])) {
             $_SESSION["username"] = $username;
             $_SESSION["name"] = $row["name"];
             $_SESSION["role"] = $row["role"];
             $_SESSION["user_id"] = $row["user_id"];
-
-            $_SESSION['notification'] = [
-                'type' => 'primary',
-                'message' => 'Selamat Datang Kembali!'
-            ];
-
+            //set notifikasi selamat datang
+            $_SESSION['notification'] = ['type' => 'primary', 'message' => 'Selamat Datang Kembali!'];
+            //redirect ke dashboard
             header('Location: ../dashboard.php');
             exit();
         } else {
-            $_SESSION['notification'] = [
-                'type' => 'danger',
-                'message' => 'Username atau Password salah'
-            ];
+            //password salah
+            $_SESSION['notification'] = ['type' => 'danger', 'message' => 'Username atau Password salah'];
         }
+    } else {
+        //username tidak ditemukan
+        $_SESSION['notification'] = ['type'  => 'danger', 'message' => 'Username atau Password salah'];
     }
+    //redirect kembali ke halaman login jika gagal
+    header('Location: login.php');
+    exit();
 }
+$conn->close();
+?>
