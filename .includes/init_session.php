75cdadf9fc5e3c1384c5ec969b8df198118fefe9
evamelisa-rpl <evamelisa2007@gmail.com>
2025-02-08 20:43:34 +0700

    menambahkan init session

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
-
+//ambil data dari sesi
+$userId = $_SESSION["user_id"];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 
 $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']) ;
+}
+/*periksa apakah sesi username dan role ada,
+jika tidak arahkan ke halaman login */
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+    $_SESSION['notification'] = [
+        'type' => 'danger',
+        'message' => 'Silakan Login Terlebih Dahulu!'
+    ];
+    header('Location: ./auth/login.php');
+    exit();
 }
