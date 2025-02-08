374a50f72972402a62ff288fecfa61720059da75
evamelisa-rpl <evamelisa2007@gmail.com>
2025-02-08 12:33:20 +0700

    menambahkan proses post

---
+++ proses_post.php
@@ -0,0 +1,51 @@
+<?php
+//menghubungkan file konfigurasi database
+include 'config.php';
+
+//mulai sesi
+session_start();
+
+//mendapatkan id pengguna
+$userId = $_SESSION["user_id"];
+
+if (isset($_POST['simpan'])) {
+    //mendapatkan data dari form
+    $postTitle = $_POST["post_title"];
+    $content = $_POST["content"];
+    $categoryId = $_POST["category_id"];
+
+    //mengatur direktori
+    $imageDir = "assests/img/uploads/";
+    $imageName = $_FILES["image"]["name"];
+    $imagePath = $imageDir . basename($imageName);
+
+    //memindahkan file gambar yang di unggah ke direktori
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)) {
+        // jika unggahan berhasil masukkan
+        // data postingan ke dalam database
+        $query = "INSERT INTO posts (post_title, content, created_at, category_id, user_id, image_path) VALUES 
+        ('$postTitle', '$content', NOW(), $categoryId, $userId, '$imagePath')";
+
+        if ($conn->query($query) === TRUE) {
+            // notifikasi berhasil jika postingan berhasil ditambahkan
+            $_SESSION['notification'] = [
+                'type' => 'primary',
+                'message' => 'Post successfully added.'
+            ];
+        } else {
+            $_SESSION['notification'] = [
+                'type' => 'danger',
+                'message' => 'Error adding post: ' . $conn->error
+            ];
+        }
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Failed to upload image.'
+        ];
+    }
+
+    // arahkan ke halaman dashboard setelah selesai
+    header('Location: dashboard.php');
+    exit();
+}
