22ccc3d5114ada36811f69ca410e5634658526a0
evamelisa-rpl <evamelisa2007@gmail.com>
2025-02-09 12:11:12 +0700

    memperbarui kode proses post

--- proses_post.php
+++ proses_post.php
@@ -8,6 +8,7 @@ session_start();
 //mendapatkan id pengguna
 $userId = $_SESSION["user_id"];
 
+
 if (isset($_POST['simpan'])) {
     //mendapatkan data dari form
     $postTitle = $_POST["post_title"];
@@ -26,19 +27,22 @@ if (isset($_POST['simpan'])) {
         $query = "INSERT INTO posts (post_title, content, created_at, category_id, user_id, image_path) VALUES 
         ('$postTitle', '$content', NOW(), $categoryId, $userId, '$imagePath')";
 
-        if ($conn->query($query) === TRUE) {
+        
+    if ($conn->query($query) === TRUE) {
             // notifikasi berhasil jika postingan berhasil ditambahkan
             $_SESSION['notification'] = [
                 'type' => 'primary',
                 'message' => 'Post successfully added.'
             ];
         } else {
+            
             $_SESSION['notification'] = [
                 'type' => 'danger',
                 'message' => 'Error adding post: ' . $conn->error
             ];
         }
     } else {
+        
         $_SESSION['notification'] = [
             'type' => 'danger',
             'message' => 'Failed to upload image.'
@@ -72,6 +76,63 @@ if (isset($_POST['delete'])) {
     }
 
     // kembali ke halaman dashboard
+    header('Location: dashboard.php');
+    exit();
+}
+
+// menangani pembaruan data
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
+    //mendapatkan data dari form
+    $postId = $_POST['post_id'];
+    $postTitle = $_POST["post_title"];
+    $content = $_POST["content"];
+    $categoryId = $_POST["category_id"];
+    $imageDir = "assets/img/uploads/";
+
+    //memeriksa file gambar
+    if (!empty($_FILES["image_path"]["name"])) {
+        $imageName = $_FILES["image_path"]["name"];
+        $imagePath = $imageDir . $imageName;
+
+        //pindahkan file baru ke direktori
+        move_uploaded_file($_FILES["image_path"]["tmp_name"], $imagePath);
+
+        //hapus gambar lama
+        $queryOldImage = "SELECT image_path FROM posts WHERE id_post = $postId";
+        $resultOldImage = $conn->query($queryOldImage);
+        if ($resultOldImage->num_rows > 0) {
+            $oldImage = $resultOldImage->fetch_assoc()['image_path'];
+            if (file_exists($oldImage)) {
+                unlink($oldImage);
+            }
+        }
+    } else {
+        // jika tidak ada file baru gunakan file lama
+        $imagePathQuery = "SELECT image_path FROM posts WHERE id_post = $postId";
+        $result = $conn->query($imagePathQuery);
+        $imagePath = ($result->num_rows > 0) ? $result->fetch_assoc()['image_path'] : null;
+    }
+
+    //update data postingan di database
+    $queryUpdate = "UPDATE posts SET post_title = '$postTitle',
+    content = '$content', category_id = $category_id,
+    image_path = '$imagePath' WHERE id_post = $postId";
+
+    if ($conn->query($queryUpdate) === TRUE) {
+        //notifikasi berhasil
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message'=>  'Postingan berhasil diperbarui.'
+        ];
+    } else {
+        //notifikasi gagal
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message'=>  'Gagal memperbarui postingan.'
+        ];
+    }
+
+    //arahkan ke dashboard
     header('Location: dashboard.php');
     exit();
 }
